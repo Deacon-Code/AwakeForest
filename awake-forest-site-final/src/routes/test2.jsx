4c1ef33b8311d16d4d882f2ae9f8cca09836112d
TylerPassyn <Tyler.A.Passyn@gmail.com>
2025-02-08 13:25:08 -0500

    Dos changes

---
+++ awake-forest-site-final/src/routes/test2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const test2 = () => {
+  return (
+    <div className='item-container'>
+        <h1> Hello World </h1>
+      
+    </div>
+  )
+}
+
+export default test2
