9e3dece7e987179b5d17b32a83f1421058bf7130
Tyler Passyn <Tyler.A.Passyn@gmail.com>
2025-07-19 19:09:12 -0500

    About Update

--- awake-forest-site-final/src/components/Description.jsx
+++ awake-forest-site-final/src/components/Description.jsx
@@ -10,10 +10,9 @@ const Description = () => {
         </div>
         <div className = "Description-text">
             <h1> About Awake Forest </h1>
-            <p> The difference between the mathematical and the intuitive mind: In the one the principles are palpable, but removed from ordinary use; so that for want of habit it is difficult to turn one's mind in that direction: but if one turns it thither ever so little, one sees the principles fully, and one must have a quite inaccurate mind who reasons wrongly from principles so plain that it is almost impossible they should escape notice.
-                But in the intuitive mind the principles are found in common use, and are before the eyes of everybody. One has only to look, and no effort is necessary; it is only a question of good eyesight, but it must be good, for the principles are so subtle and so numerous, that it is almost impossible but that some escape notice. Now the omission of one principle leads to error; thus one must have very clear sight to see all the principles, and in the next place an accurate mind not to draw false deductions from known principles.
-                All mathematicians would then be intuitive if they had clear sight, for they do not reason incorrectly from principles known to them; and intuitive minds would be mathematical if they could turn their eyes to the principles of mathematics to which they are unused.
-            </p>
+            <p>  
+              If you’ve ever wondered why your heart drops when you forget to bring your phone to the bathroom, why your mind starts racing as soon as you lay down for bed, or why “Who Am I?” can be such a difficult yet beautiful question to explore, swing by for a sit, a yoga sesh, a guest speaker, or anything at all, we’d love to meet you. Welcome to Awake Forest - We’re an ever-evolving community of students impassioned by this life, with the intention to live it joyfully, peacefully, and authentically, together. Whether meditating, playing hacky sack, getting Pit, or running through the rain, Awake Forest is a group of students exploring what it means to be our true selves and live life to the fullest. 
+          </p>
         </div>
     </div>
   )
